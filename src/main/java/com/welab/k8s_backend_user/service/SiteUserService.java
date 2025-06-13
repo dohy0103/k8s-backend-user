48d027ec850b0a825aee86f6b6c9adb1f538df1a
dohy0103 <kdohy0103@gmail.com>
2025-06-13 16:19:53 +0900

    db & login

--- src/main/java/com/welab/k8s_backend_user/service/SiteUserService.java
+++ src/main/java/com/welab/k8s_backend_user/service/SiteUserService.java
@@ -1,10 +1,16 @@
 package com.welab.k8s_backend_user.service;
 
+import com.welab.k8s_backend_user.common.exception.BadParameter;
+import com.welab.k8s_backend_user.common.exception.NotFound;
 import com.welab.k8s_backend_user.domain.SiteUser;
+import com.welab.k8s_backend_user.domain.dto.SiteUserLoginDto;
 import com.welab.k8s_backend_user.domain.dto.SiteUserRegisterDto;
 import com.welab.k8s_backend_user.domain.event.SiteUserInfoEvent;
 import com.welab.k8s_backend_user.domain.repository.SiteUserRepository;
 import com.welab.k8s_backend_user.event.producer.KafkaMessageProducer;
+import com.welab.k8s_backend_user.secret.hash.SecureHashUtils;
+import com.welab.k8s_backend_user.secret.jwt.TokenGenerator;
+import com.welab.k8s_backend_user.secret.jwt.dto.TokenDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +22,7 @@ import org.springframework.stereotype.Service;
 public class SiteUserService {
     private final SiteUserRepository siteUserRepository;
     private final KafkaMessageProducer kafkaMessageProducer;
+    private final TokenGenerator tokenGenerator;
 
     @Transactional
     public void registerUser(SiteUserRegisterDto registerDto) {
@@ -25,5 +32,19 @@ public class SiteUserService {
 
         SiteUserInfoEvent event = SiteUserInfoEvent.fromEntity("Create", siteUser);
         kafkaMessageProducer.send(SiteUserInfoEvent.Topic, event);
+    }
+
+    @Transactional
+    public TokenDto.AccessRefreshToken login(SiteUserLoginDto loginDto) {
+        SiteUser user = siteUserRepository.findByUserId(loginDto.getUserId());
+        if (user == null) {
+            throw new NotFound("아이디 또는 비밀번호를 확인하세요.");
+        }
+
+        if (!SecureHashUtils.matches(loginDto.getPassword(), user.getPassword())) {
+            throw new BadParameter("아이디 또는 비밀번호를 확인하세요.");
+        }
+
+        return tokenGenerator.generateAccessRefreshToken(loginDto.getUserId(), "WEB");
     }
 }
